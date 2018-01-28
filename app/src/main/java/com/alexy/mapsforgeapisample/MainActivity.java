eebeebeffe6704546274734e37a0977377cdb341
Alexey Krichun <alexey.krichun@mapp.com>
2018-01-29 00:38:31 +0200

    Vector renderer

--- app/src/main/java/com/alexy/mapsforgeapisample/MainActivity.java
+++ app/src/main/java/com/alexy/mapsforgeapisample/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 
 import com.alexy.mapsforgeapilibrary.MapsforgeAPI;
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST = 5555;
 
     private MapsforgeAPI mapsforgeAPI;
+    private CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,24 +24,62 @@ public class MainActivity extends AppCompatActivity {
 
     private void showMap() {
         mapsforgeAPI = (MapsforgeAPI)findViewById(R.id.mapView);
+        checkBox = (CheckBox)findViewById(R.id.animCheckBox);
 
         findViewById(R.id.buttonMinus).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mapsforgeAPI.zoomOut(true);
+                mapsforgeAPI.zoomOut(isAnimated());
             }
         });
 
         findViewById(R.id.buttonPlus).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mapsforgeAPI.zoomIn(true);
+                mapsforgeAPI.zoomIn(isAnimated());
+            }
+        });
+
+        findViewById(R.id.buttonZoom).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mapsforgeAPI.zoomTo(12, isAnimated());
+            }
+        });
+
+        findViewById(R.id.buttonLeft).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mapsforgeAPI.rotate(-Math.PI/4, 0,0, isAnimated());
+            }
+        });
+
+        findViewById(R.id.buttonRight).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mapsforgeAPI.rotate(Math.PI/4, 0,0, isAnimated());
             }
         });
 
         mapsforgeAPI.loadMap(Environment.getExternalStorageDirectory().getAbsolutePath() + "/massachusetts.map");
         mapsforgeAPI.moveTo(42.358056, -71.063611);
         mapsforgeAPI.zoomTo(10, false);
+
+        mapsforgeAPI.setOnTapListener(new MapsforgeAPI.OnTapListener() {
+            @Override
+            public boolean onTap(double lat, double lon) {
+                if (isAnimated()) {
+                    mapsforgeAPI.animateTo(lat, lon);
+                } else {
+                    mapsforgeAPI.moveTo(lat, lon);
+                }
+                return true;
+            }
+        });
+    }
+
+    private boolean isAnimated() {
+        return checkBox.isChecked();
     }
 
 
@@ -54,6 +94,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapsforgeAPI.destroy();
+    }
 
     private void requestPermissions() {
         if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
